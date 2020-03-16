e1b1c53c23dcdd13ccbfcca3535348c830377db6
jadamcrain <jadamcrain@gmail.com>
2020-03-16 14:52:28 -0700

    test sizes

--- src/app/header.rs
+++ src/app/header.rs
@@ -10,6 +10,7 @@ where
     fn parse(cursor: &mut ReadCursor) -> Result<Self, ReadError>;
 }
 
+#[repr(transparent)]
 pub struct Group2Var1Seq<'a> {
     marker: RangeMarker<'a>,
 }
@@ -19,7 +20,7 @@ impl<'a> Group2Var1Seq<'a> {
         self.marker.iter()
     }
 }
-
+#[repr(transparent)]
 pub struct Group2Var2Seq<'a> {
     marker: RangeMarker<'a>,
 }
@@ -29,7 +30,7 @@ impl<'a> Group2Var2Seq<'a> {
         self.marker.iter()
     }
 }
-
+#[repr(transparent)]
 pub struct Group2Var3Seq<'a> {
     marker: RangeMarker<'a>,
 }
@@ -44,7 +45,7 @@ pub enum RangedVariation<'a> {
     Group2Var0,
     Group2Var1(Group2Var1Seq<'a>),
     Group2Var2(Group2Var2Seq<'a>),
-    Group2Var3(Group2Var3Seq<'a>),
+    //Group2Var3(Group2Var3Seq<'a>),
 }
 
 pub enum Header<'a> {
@@ -58,15 +59,17 @@ mod test {
 
     #[test]
     fn iterates_over_g2v1() {
+
+        println!("header: {}", std::mem::size_of::<Header>());
+        println!("header[64]: {}", std::mem::size_of::<[Header; 128]>());
+
         let seq = Group2Var1Seq {
-            marker: RangeMarker::new(1, 2, &[0xAA, 0xBB]),
+            marker: RangeMarker::new(1, &[0xAA, 0xBB]),
         };
 
         let items: Vec<(Group2Var1, u16)> = seq.iter().collect();
         assert_eq!(items.len(), 2);
         assert_eq!(items[0], (Group2Var1 { flags: 0xAA }, 1));
         assert_eq!(items[1], (Group2Var1 { flags: 0xBB }, 2));
-
-        seq.iter().len();
     }
 }
--- src/app/range.rs
+++ src/app/range.rs
@@ -3,13 +3,12 @@ use crate::util::cursor::ReadCursor;
 
 pub struct RangeMarker<'a> {
     start: u16,
-    count: usize,
     data: &'a [u8],
 }
 
 impl<'a> RangeMarker<'a> {
-    pub fn new(start: u16, count: usize, data: &'a [u8]) -> Self {
-        Self { start, count, data }
+    pub fn new(start: u16, data: &'a [u8]) -> Self {
+        Self { start, data }
     }
 }
 
@@ -20,7 +19,6 @@ impl<'a> RangeMarker<'a> {
     {
         RangeIterator {
             index: self.start,
-            count: self.count,
             cursor: ReadCursor::new(self.data),
             phantom: std::marker::PhantomData {},
         }
@@ -29,19 +27,8 @@ impl<'a> RangeMarker<'a> {
 
 pub struct RangeIterator<'a, T> {
     index: u16,
-    count: usize,
     cursor: ReadCursor<'a>,
     phantom: std::marker::PhantomData<T>,
-}
-
-impl<'a, T> RangeIterator<'a, T> {
-    pub fn len(&self) -> usize {
-        self.count
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.count == 0
-    }
 }
 
 impl<'a, T> Iterator for RangeIterator<'a, T>
