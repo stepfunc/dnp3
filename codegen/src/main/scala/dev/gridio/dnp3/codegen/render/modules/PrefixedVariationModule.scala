1ce47eb603743e5cad722760a3fca95611fdbea8
jadamcrain <jadamcrain@gmail.com>
2020-03-23 14:12:22 -0700

    handle parsing of Group111

--- codegen/src/main/scala/dev/gridio/dnp3/codegen/render/modules/PrefixedVariationModule.scala
+++ codegen/src/main/scala/dev/gridio/dnp3/codegen/render/modules/PrefixedVariationModule.scala
@@ -1,6 +1,7 @@
 package dev.gridio.dnp3.codegen.render.modules
 
 import dev.gridio.dnp3.codegen.model._
+import dev.gridio.dnp3.codegen.model.groups.{Group111, Group111AnyVar}
 import dev.gridio.dnp3.codegen.render._
 
 object PrefixedVariationModule extends Module {
@@ -13,6 +14,7 @@ object PrefixedVariationModule extends Module {
       "use crate::app::parser::ParseError;".eol ++
       "use crate::app::header::FixedSize;".eol ++
       "use crate::app::prefix::Prefix;".eol ++
+      "use crate::app::bytes::PrefixedBytesSequence;".eol ++
       space ++
       enumDefinition ++
       space ++
@@ -21,22 +23,37 @@ object PrefixedVariationModule extends Module {
 
   private def enumDefinition(implicit indent: Indentation) : Iterator[String] = {
 
+    def definition(v : Variation) : Iterator[String] = v match {
+      case _ : SizedByVariation =>{
+        "Group111VarX(u8, PrefixedBytesSequence<'a, I>),".eol
+      }
+      case _ => s"${v.name}(CountSequence<'a, Prefix<I, ${v.name}>>),".eol
+    }
+
     "#[derive(Debug, PartialEq)]".eol ++
       bracket("pub enum PrefixedVariation<'a, I> where I : FixedSize") {
-        variations.iterator.map(v => s"${v.name}(CountSequence<'a, Prefix<I, ${v.name}>>),")
+        variations.iterator.flatMap(definition)
       }
 
   }
 
   private def enumImpl(implicit indent: Indentation) : Iterator[String] = {
 
+    def matcher(v: Variation) : Iterator[String] = v match {
+      case Group111AnyVar => {
+        "Variation::Group111(0) => Err(ParseError::ZeroLengthOctetData),".eol ++
+        "Variation::Group111(x) => Ok(PrefixedVariation::Group111VarX(x, PrefixedBytesSequence::parse(x, count, cursor)?)),".eol
+      }
+      case _ => {
+        s"Variation::${v.name} => Ok(PrefixedVariation::${v.name}(CountSequence::parse(count, cursor)?)),".eol
+      }
+    }
+
     bracket("impl<'a, I> PrefixedVariation<'a, I> where I : FixedSize") {
       "#[rustfmt::skip]".eol ++
       bracket("pub fn parse(v: Variation, count: u16, cursor: &mut ReadCursor<'a>) -> Result<PrefixedVariation<'a, I>, ParseError>") {
         bracket("match v") {
-          variations.map { v =>
-            s"Variation::${v.name} => Ok(PrefixedVariation::${v.name}(CountSequence::parse(count, cursor)?)),"
-          } ++ "_ => Err(ParseError::InvalidQualifierForVariation(v)),".eol
+          variations.flatMap(matcher) ++ "_ => Err(ParseError::InvalidQualifierForVariation(v)),".eol
         }
       }
     }
@@ -45,6 +62,7 @@ object PrefixedVariationModule extends Module {
 
   def variations : Iterator[Variation] = {
     ObjectGroup.allVariations.iterator.collect {
+      case v : SizedByVariation if v.parent == Group111 => v
       case v : FixedSize if v.parent.groupType == GroupType.Event || v.parent.groupType == GroupType.Command => v
     }
   }
