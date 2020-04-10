f418c31cb774660ff83dffbe1230eaef14d91643
jadamcrain <jadamcrain@gmail.com>
2020-04-09 18:14:15 -0700

    make a few more transport items private

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -14,15 +14,14 @@ pub type ReaderType = crate::transport::mocks::MockReader;
 #[cfg(test)]
 pub type WriterType = crate::transport::mocks::MockWriter;
 
-pub mod header;
+pub(crate) mod header;
 pub mod reader;
-pub mod sequence;
+pub(crate) mod sequence;
 pub mod writer;
 
-pub mod constants {
-    pub const FIN_MASK: u8 = 0b1000_0000;
-    pub const FIR_MASK: u8 = 0b0100_0000;
-    //pub const SEQ_MASK: u8 = 0b0011_1111;
+pub(crate) mod constants {
+    pub(crate) const FIN_MASK: u8 = 0b1000_0000;
+    pub(crate) const FIR_MASK: u8 = 0b0100_0000;
 }
 
 pub fn create_transport_layer(is_master: bool, address: u16) -> (ReaderType, WriterType) {
