64e8623478409fd56801a32b023cc67fe79c2b44
Émile Grégoire <eg@emilegregoire.ca>
2020-06-17 15:39:57 -0400

    Add constructor for group 41.

--- src/app/extensions.rs
+++ src/app/extensions.rs
@@ -1,6 +1,6 @@
 use crate::app::enums::{CommandStatus, OpType};
 use crate::app::types::ControlCode;
-use crate::app::variations::Group12Var1;
+use crate::app::variations::{Group12Var1, Group41Var1, Group41Var2, Group41Var3, Group41Var4};
 
 impl Group12Var1 {
     /// construct a `Group12Var1` instance. The status field is automatically set to `CommandStatus::Success`
@@ -40,6 +40,46 @@ impl Group12Var1 {
             count: 1,
             on_time: 1000,
             off_time: 1000,
+            status: CommandStatus::Success,
+        }
+    }
+}
+
+impl Group41Var1 {
+    /// construct a `Group41Var1` instance. The status field is automatically set to `CommandStatus::Success`
+    pub fn new(value: i32) -> Self {
+        Self {
+            value,
+            status: CommandStatus::Success,
+        }
+    }
+}
+
+impl Group41Var2 {
+    /// construct a `Group41Var2` instance. The status field is automatically set to `CommandStatus::Success`
+    pub fn new(value: i16) -> Self {
+        Self {
+            value,
+            status: CommandStatus::Success,
+        }
+    }
+}
+
+impl Group41Var3 {
+    /// construct a `Group41Var3` instance. The status field is automatically set to `CommandStatus::Success`
+    pub fn new(value: f32) -> Self {
+        Self {
+            value,
+            status: CommandStatus::Success,
+        }
+    }
+}
+
+impl Group41Var4 {
+    /// construct a `Group41Var4` instance. The status field is automatically set to `CommandStatus::Success`
+    pub fn new(value: f64) -> Self {
+        Self {
+            value,
             status: CommandStatus::Success,
         }
     }
