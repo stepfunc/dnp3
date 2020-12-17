6b12028ffe87a97909f2fa08aa2e879e51d4fe00
jadamcrain <jadamcrain@gmail.com>
2020-12-17 15:52:12 -0800

    first test case for confirming and clearing unsolicited data

--- dnp3/src/outstation/tests/unsolicited.rs
+++ dnp3/src/outstation/tests/unsolicited.rs
@@ -1,10 +1,44 @@
 use super::harness::*;
+use crate::app::flags::Flags;
+use crate::app::measurement::{Binary, Time};
 use crate::outstation::config::OutstationConfig;
+use crate::outstation::database::config::BinaryConfig;
+use crate::outstation::database::{Add, DatabaseHandle, EventClass, Update, UpdateOptions};
 use crate::util::task::RunError;
+
+const fn uns_confirm(seq: u8) -> u8 {
+    0b1101_0000 | seq
+}
 
 const NULL_UNSOL_SEQ_0: &[u8] = &[0xF0, 0x82, 0x80, 0x00];
 const NULL_UNSOL_SEQ_1: &[u8] = &[0xF1, 0x82, 0x80, 0x00];
-const UNS_CONFIRM_SEQ_0: &[u8] = &[0b1101_0000, 0x00];
+const UNS_CONFIRM_SEQ_0: &[u8] = &[uns_confirm(0), 0x00];
+const UNS_CONFIRM_SEQ_1: &[u8] = &[uns_confirm(1), 0x00];
+const UNSOL_G2V1_SEQ1: &[u8] = &[
+    0xF1, 0x82, 0x80, 0x00, 0x02, 0x01, 0x28, 0x01, 0x00, 0x00, 0x00, 0x81,
+];
+const ENABLE_UNSOLICITED_SEQ0: &[u8] = &[
+    0xC0, 0x14, 0x3C, 0x02, 0x06, 0x3C, 0x03, 0x06, 0x3C, 0x04, 0x06,
+];
+const EMPTY_RESPONSE_SEQ0: &[u8] = &[0xC0, 0x81, 0x80, 0x00];
+
+fn generate_binary_event(handle: &mut DatabaseHandle) {
+    handle.transaction(|db| {
+        db.add(0, Some(EventClass::Class1), BinaryConfig::default());
+        db.update(
+            0,
+            &Binary::new(true, Flags::ONLINE, Time::synchronized(0)),
+            UpdateOptions::default(),
+        )
+    });
+}
+
+fn enable_unsolicited<T>(harness: &mut OutstationTestHarness<T>)
+where
+    T: std::future::Future<Output = Result<(), RunError>>,
+{
+    harness.test_request_response(ENABLE_UNSOLICITED_SEQ0, EMPTY_RESPONSE_SEQ0);
+}
 
 fn confirm_null_unsolicited<T>(harness: &mut OutstationTestHarness<T>)
 where
@@ -74,7 +108,14 @@ fn null_unsolicited_can_timeout_series_wait_and_start_another_series() {
 }
 
 #[test]
-fn null_unsolicited_can_be_confirmed() {
+fn data_unsolicited_can_be_confirmed() {
     let mut harness = new_harness(get_default_unsolicited_config());
     confirm_null_unsolicited(&mut harness);
+    enable_unsolicited(&mut harness);
+
+    generate_binary_event(&mut harness.database);
+    harness.expect_response(UNSOL_G2V1_SEQ1);
+    harness.check_events(&[Event::EnterUnsolicitedConfirmWait(1)]);
+    harness.send(UNS_CONFIRM_SEQ_1);
+    harness.check_events(&[Event::UnsolicitedConfirmReceived(1)]);
 }
