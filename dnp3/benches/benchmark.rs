644a628f53205e9faf61ea41e3b7f3d6c1820389
jadamcrain <jadamcrain@gmail.com>
2021-01-14 11:42:08 -0800

    working benchmark for a single pair

--- dnp3/benches/benchmark.rs
+++ dnp3/benches/benchmark.rs
@@ -1,10 +1,350 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use dnp3::app::measurement::{Analog, Binary, Counter, Time};
-use dnp3::outstation::database::{Database, EventMode, Update, UpdateOptions};
+use criterion::{criterion_group, criterion_main, Criterion};
+use dnp3::app::measurement::{
+    Analog, AnalogOutputStatus, Binary, BinaryOutputStatus, Counter, DoubleBitBinary,
+    FrozenCounter, Time,
+};
+use dnp3::outstation::database::{
+    Add, Database, DatabaseConfig, EventBufferConfig, EventClass, EventMode, Update, UpdateOptions,
+};
 
 use dnp3::app::flags::Flags;
+use dnp3::app::header::ResponseHeader;
+use dnp3::app::parse::bytes::Bytes;
+use dnp3::app::parse::DecodeLogLevel;
+use dnp3::app::retry::{ReconnectStrategy, RetryStrategy};
+use dnp3::app::timeout::Timeout;
 use dnp3::app::types::Timestamp;
+use dnp3::entry::master::tcp::EndpointList;
+use dnp3::entry::outstation::tcp::{ServerHandle, TCPServer};
+use dnp3::entry::outstation::AddressFilter;
+use dnp3::entry::EndpointAddress;
+use dnp3::master::association::Configuration;
+use dnp3::master::handle::{
+    AssociationHandler, HeaderInfo, Listener, MasterConfiguration, MasterHandle, ReadHandler,
+};
+use dnp3::master::request::EventClasses;
+use dnp3::outstation::config::OutstationConfig;
+use dnp3::outstation::database::config::{
+    AnalogConfig, BinaryConfig, CounterConfig, EventAnalogVariation, EventBinaryVariation,
+    EventCounterVariation, StaticAnalogVariation, StaticBinaryVariation, StaticCounterVariation,
+};
+use dnp3::outstation::traits::{
+    DefaultControlHandler, DefaultOutstationApplication, DefaultOutstationInformation,
+};
+use dnp3::outstation::OutstationHandle;
 use rand::{Rng, SeedableRng};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+fn config() -> TestConfig {
+    TestConfig {
+        outstation_level: DecodeLogLevel::Nothing,
+        master_level: DecodeLogLevel::Nothing,
+        num_values: 100,
+        max_index: 10,
+    }
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .init();
+
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let mut pair = runtime.block_on(Pair::spawn(20000, config()));
+
+    runtime.block_on(pair.wait_for_null_unsolicited());
+
+    c.bench_function("outstation", |b| {
+        b.iter(|| {
+            pair.update_values();
+
+            runtime.block_on(pair.wait_for_update());
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
+
+#[derive(Copy, Clone)]
+struct TestConfig {
+    outstation_level: DecodeLogLevel,
+    master_level: DecodeLogLevel,
+    num_values: usize,
+    max_index: u16,
+}
+
+struct Pair {
+    // measurements exchanged on each iteration
+    values: Measurements,
+    // have to hold onto this to keep TCP server alive
+    _server: ServerHandle,
+    // have to hold onto this to keep master alive
+    _master: MasterHandle,
+    // count of matching measurements received
+    rx: tokio::sync::mpsc::Receiver<usize>,
+    // used to update the database
+    outstation: OutstationHandle,
+}
+
+impl Pair {
+    const LOCALHOST: std::net::IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+
+    fn update_values(&mut self) {
+        self.outstation.database.transaction(|db| {
+            for x in self.values.once() {
+                x.apply(db)
+            }
+        })
+    }
+
+    async fn wait_for_null_unsolicited(&mut self) {
+        assert_eq!(self.rx.recv().await.unwrap(), 0);
+    }
+
+    async fn wait_for_update(&mut self) {
+        assert_eq!(self.rx.recv().await.unwrap(), self.values.len());
+    }
+
+    async fn spawn(port: u16, config: TestConfig) -> Self {
+        let (server, outstation) = Self::spawn_outstation(port, config).await;
+        let (master, measurements, rx) = Self::spawn_master(port, config).await;
+
+        Self {
+            values: measurements,
+            _server: server,
+            rx,
+            _master: master,
+            outstation,
+        }
+    }
+
+    async fn spawn_outstation(port: u16, config: TestConfig) -> (ServerHandle, OutstationHandle) {
+        let mut server = TCPServer::new(SocketAddr::new(Self::LOCALHOST, port));
+        let (outstation, task) = server
+            .add_outstation(
+                Self::get_outstation_config(config.outstation_level),
+                Self::get_database_config(),
+                DefaultOutstationApplication::create(),
+                DefaultOutstationInformation::create(),
+                DefaultControlHandler::create(),
+                AddressFilter::Any,
+            )
+            .unwrap();
+        tokio::spawn(task);
+
+        // set up the database
+        outstation.database.transaction(|db| {
+            for i in 0..=config.max_index {
+                db.add(
+                    i,
+                    Some(EventClass::Class1),
+                    BinaryConfig::new(
+                        StaticBinaryVariation::Group1Var2,
+                        EventBinaryVariation::Group2Var2,
+                    ),
+                );
+                db.add(
+                    i,
+                    Some(EventClass::Class1),
+                    CounterConfig::new(
+                        StaticCounterVariation::Group20Var2,
+                        EventCounterVariation::Group22Var5,
+                        0,
+                    ),
+                );
+                db.add(
+                    i,
+                    Some(EventClass::Class1),
+                    AnalogConfig::new(
+                        StaticAnalogVariation::Group30Var1,
+                        EventAnalogVariation::Group32Var3,
+                        0.0,
+                    ),
+                );
+            }
+        });
+
+        let (server_handle, task) = server.bind().await.unwrap();
+        tokio::spawn(task);
+
+        (server_handle, outstation)
+    }
+
+    async fn spawn_master(
+        port: u16,
+        config: TestConfig,
+    ) -> (
+        MasterHandle,
+        Measurements,
+        tokio::sync::mpsc::Receiver<usize>,
+    ) {
+        let mut master = dnp3::entry::master::tcp::spawn_master_tcp_client(
+            Self::get_master_config(config.master_level),
+            EndpointList::single(format!("127.0.0.1:{}", port)),
+            Listener::None,
+        );
+
+        let measurements = Measurements::new(config.max_index, config.num_values);
+        let (tx, rx) = tokio::sync::mpsc::channel(16);
+
+        let handler = TestHandler {
+            count: 0,
+            measurements: measurements.forever(),
+            tx,
+        };
+
+        // don't are about the handle
+        let _ = master
+            .add_association(
+                Self::outstation_address(),
+                Self::get_association_config(),
+                Box::new(handler),
+            )
+            .await
+            .unwrap();
+
+        (master, measurements, rx)
+    }
+
+    fn outstation_address() -> EndpointAddress {
+        EndpointAddress::from(10).unwrap()
+    }
+
+    fn master_address() -> EndpointAddress {
+        EndpointAddress::from(1).unwrap()
+    }
+
+    fn get_master_config(level: DecodeLogLevel) -> MasterConfiguration {
+        MasterConfiguration::new(
+            Self::master_address(),
+            level,
+            ReconnectStrategy::default(),
+            Timeout::from_secs(5).unwrap(),
+        )
+    }
+
+    fn get_association_config() -> Configuration {
+        let mut config = Configuration::quiet(RetryStrategy::default());
+        config.enable_unsol_classes = EventClasses::all();
+        config
+    }
+
+    fn get_outstation_config(level: DecodeLogLevel) -> OutstationConfig {
+        let mut config = OutstationConfig::new(Self::outstation_address(), Self::master_address());
+        config.log_level = level;
+        config
+    }
+
+    fn get_database_config() -> DatabaseConfig {
+        let mut config = DatabaseConfig::default();
+        config.events = EventBufferConfig::all_types(100);
+        config
+    }
+}
+
+struct TestHandler {
+    count: usize,
+    measurements: CyclicMeasurementIterator,
+    tx: tokio::sync::mpsc::Sender<usize>,
+}
+
+impl ReadHandler for TestHandler {
+    fn begin_fragment(&mut self, _header: ResponseHeader) {
+        self.count = 0;
+    }
+
+    fn end_fragment(&mut self, _header: ResponseHeader) {
+        self.tx.try_send(self.count).unwrap();
+    }
+
+    fn handle_binary(&mut self, _info: HeaderInfo, iter: &mut dyn Iterator<Item = (Binary, u16)>) {
+        for (v, i) in iter {
+            if self.measurements.expect(Measurement::Binary(v, i)) {
+                self.count += 1;
+            }
+        }
+    }
+
+    fn handle_double_bit_binary(
+        &mut self,
+        _info: HeaderInfo,
+        _iter: &mut dyn Iterator<Item = (DoubleBitBinary, u16)>,
+    ) {
+        unimplemented!()
+    }
+
+    fn handle_binary_output_status(
+        &mut self,
+        _info: HeaderInfo,
+        _iter: &mut dyn Iterator<Item = (BinaryOutputStatus, u16)>,
+    ) {
+        unimplemented!()
+    }
+
+    fn handle_counter(
+        &mut self,
+        _info: HeaderInfo,
+        iter: &mut dyn Iterator<Item = (Counter, u16)>,
+    ) {
+        for (v, i) in iter {
+            if self.measurements.expect(Measurement::Counter(v, i)) {
+                self.count += 1;
+            }
+        }
+    }
+
+    fn handle_frozen_counter(
+        &mut self,
+        _info: HeaderInfo,
+        _iter: &mut dyn Iterator<Item = (FrozenCounter, u16)>,
+    ) {
+        unimplemented!()
+    }
+
+    fn handle_analog(&mut self, _info: HeaderInfo, iter: &mut dyn Iterator<Item = (Analog, u16)>) {
+        for (v, i) in iter {
+            if self.measurements.expect(Measurement::Analog(v, i)) {
+                self.count += 1;
+            }
+        }
+    }
+
+    fn handle_analog_output_status(
+        &mut self,
+        _info: HeaderInfo,
+        _iter: &mut dyn Iterator<Item = (AnalogOutputStatus, u16)>,
+    ) {
+        unimplemented!()
+    }
+
+    fn handle_octet_string<'a>(
+        &mut self,
+        _info: HeaderInfo,
+        _iter: &'a mut dyn Iterator<Item = (Bytes<'a>, u16)>,
+    ) {
+        unimplemented!()
+    }
+}
+
+impl AssociationHandler for TestHandler {
+    fn get_integrity_handler(&mut self) -> &mut dyn ReadHandler {
+        self
+    }
+
+    fn get_unsolicited_handler(&mut self) -> &mut dyn ReadHandler {
+        self
+    }
+
+    fn get_default_poll_handler(&mut self) -> &mut dyn ReadHandler {
+        self
+    }
+}
 
 // don't need to send every type
 #[derive(Copy, Clone, PartialEq, Debug)]
@@ -79,43 +419,51 @@ struct Measurements {
 }
 
 impl Measurements {
-    fn iterator(&self) -> MeasurementIterator {
-        MeasurementIterator {
+    fn len(&self) -> usize {
+        self.inner.len()
+    }
+    fn new(max_index: u16, count: usize) -> Self {
+        let mut rand = Random::new(max_index);
+        let mut values = Vec::new();
+        for _ in 0..count {
+            values.push(rand.measurement());
+        }
+        Self {
+            inner: std::sync::Arc::new(values),
+        }
+    }
+
+    fn once(&self) -> impl Iterator<Item = &Measurement> {
+        self.inner.iter()
+    }
+
+    fn forever(&self) -> CyclicMeasurementIterator {
+        CyclicMeasurementIterator {
             next: 0,
             list: self.clone(),
         }
     }
 }
 
-struct MeasurementIterator {
+struct CyclicMeasurementIterator {
     next: usize,
     list: Measurements,
 }
 
-impl MeasurementIterator {
+impl CyclicMeasurementIterator {
     fn next(&mut self) -> Measurement {
         let index = self.next;
         self.next = (self.next + 1) % self.list.inner.len();
         self.list.inner[index]
     }
-}
 
-fn generate_measurements(max_index: u16, count: usize) -> Measurements {
-    let mut rand = Random::new(max_index);
-    let mut values = Vec::new();
-    for _ in 0..count {
-        values.push(rand.measurement());
+    fn expect(&mut self, actual: Measurement) -> bool {
+        let expected = self.next();
+        if expected == actual {
+            true
+        } else {
+            tracing::error!("expected: {:?} actual: {:?}", expected, actual);
+            false
+        }
     }
-    Measurements {
-        inner: std::sync::Arc::new(values),
-    }
 }
-
-fn fibonacci(_: u32) {}
-
-pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
-}
-
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
