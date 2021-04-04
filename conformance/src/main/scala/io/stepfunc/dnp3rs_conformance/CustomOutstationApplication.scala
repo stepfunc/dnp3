f18cd5f8abe9fc8256f75a226b0b71389f09a843
jadamcrain <jadamcrain@gmail.com>
2021-04-04 15:06:47 -0700

    update conformance harness

--- conformance/src/main/scala/io/stepfunc/dnp3rs_conformance/CustomOutstationApplication.scala
+++ conformance/src/main/scala/io/stepfunc/dnp3rs_conformance/CustomOutstationApplication.scala
@@ -30,6 +30,36 @@ class CustomOutstationApplication(val isLocalControl: Boolean) extends Outstatio
 
   override def warmRestart: RestartDelay = RestartDelay.notSupported()
 
+  override def freezeCountersAll(freezeType: FreezeType, database: Database): FreezeResult = {
+    for (i <- 0 to 9) {
+      val currentCounter = database.getCounter(ushort(i))
+      database.updateFrozenCounter(new FrozenCounter(currentCounter.index, currentCounter.value, currentCounter.flags, currentCounter.time), new UpdateOptions())
+      if (freezeType == FreezeType.FREEZE_AND_CLEAR) {
+        currentCounter.value = uint(0)
+        database.updateCounter(currentCounter, new UpdateOptions())
+      }
+    }
+
+    FreezeResult.SUCCESS
+  }
+
+  override def freezeCountersRange(start: UShort, stop: UShort, freezeType: FreezeType, database: Database): FreezeResult = {
+    if (start.intValue() > 9 || stop.intValue() > 9) {
+      return FreezeResult.PARAMETER_ERROR
+    }
+
+    for (i <- start.intValue() to stop.intValue()) {
+      val currentCounter = database.getCounter(ushort(i))
+      database.updateFrozenCounter(new FrozenCounter(currentCounter.index, currentCounter.value, currentCounter.flags, currentCounter.time), new UpdateOptions())
+      if (freezeType == FreezeType.FREEZE_AND_CLEAR) {
+        currentCounter.value = uint(0)
+        database.updateCounter(currentCounter, new UpdateOptions())
+      }
+    }
+
+    FreezeResult.SUCCESS
+  }
+
   def now(): Timestamp = {
     val time = this.lastTimestamp.plus(Duration.between(this.lastUpdate, Instant.now))
     val quality = if (isTimeValid) TimeQuality.SYNCHRONIZED else TimeQuality.NOT_SYNCHRONIZED
@@ -43,4 +73,6 @@ class CustomOutstationApplication(val isLocalControl: Boolean) extends Outstatio
   private def needsTime: Boolean = {
     Duration.between(this.lastUpdate, Instant.now).compareTo(refreshRate.dividedBy(2)) > 0
   }
+
+
 }
