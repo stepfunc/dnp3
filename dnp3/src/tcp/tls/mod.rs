75ade8b346586f2ec4f90ecc734bdf70a8344803
Émile Grégoire <eg@emilegregoire.ca>
2021-11-04 16:08:27 -0400

    Add Common Name parsing if no SAN are present.

--- dnp3/src/tcp/tls/mod.rs
+++ dnp3/src/tcp/tls/mod.rs
@@ -1,12 +1,13 @@
 mod master;
 mod outstation;
 
+use std::convert::TryFrom;
 use std::io::{self, ErrorKind};
 use std::path::Path;
 
 pub use master::*;
 pub use outstation::*;
-use tokio_rustls::rustls;
+use tokio_rustls::{rustls, webpki};
 
 /// Certificate validation mode
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
@@ -70,6 +71,99 @@ impl MinTlsVersion {
             Self::Tls1_2 => MIN_TLS12_VERSIONS,
             Self::Tls1_3 => MIN_TLS13_VERSIONS,
         }
+    }
+}
+
+fn verify_dns_name(cert: &rustls::Certificate, server_name: &str) -> Result<(), rustls::Error> {
+    // Extract the DNS name
+    /*let dns_name_str = match server_name {
+        rustls::ServerName::DnsName(dns_name) => dns_name.as_ref(),
+        &_ => {
+            // I don't undertand why I need this, but the compiler keeps complaining otherwise
+            unreachable!()
+        }
+    };*/
+    let dns_name = webpki::DnsNameRef::try_from_ascii_str(server_name)
+        .map_err(|_| rustls::Error::InvalidCertificateData("invalid DNS name".to_string()))?;
+
+    // Let webpki parse the cert
+    let end_entity_cert = webpki::EndEntityCert::try_from(cert.0.as_ref()).map_err(pki_error)?;
+
+    // Try validating the name using webpki. This only checks SAN extensions
+    match end_entity_cert.verify_is_valid_for_dns_name(dns_name) {
+        Ok(()) => Ok(()), // Good, we found a SAN extension that fits for the DNS name
+        Err(webpki::Error::CertNotValidForName) => {
+            // Let's extend our search to the CN
+            // Parse the certificate using rasn
+            let parsed_cert = rasn::x509::Certificate::parse(&cert.0).map_err(|err| {
+                rustls::Error::InvalidCertificateData(format!(
+                    "unable to parse cert with rasn: {:?}",
+                    err
+                ))
+            })?;
+
+            // Parse the extensions (if present) and check that no SAN are present
+            if let Some(extensions) = &parsed_cert.tbs_certificate.value.extensions {
+                // Parse the extensions
+                let extensions = extensions.parse().map_err(|err| {
+                    rustls::Error::InvalidCertificateData(format!(
+                        "unable to parse certificate extensions with rasn: {:?}",
+                        err
+                    ))
+                })?;
+
+                // Check that no SAN extension are present
+                if extensions.iter().any(|x| {
+                    matches!(
+                        x.content,
+                        rasn::extensions::SpecificExtension::SubjectAlternativeName(_)
+                    )
+                }) {
+                    return Err(rustls::Error::InvalidCertificateData(
+                        "certificate not valid for name, SAN extensions do not match".to_string(),
+                    ));
+                }
+            }
+
+            // Parse the cert subject
+            let subject = parsed_cert
+                .tbs_certificate
+                .value
+                .subject
+                .parse()
+                .map_err(|err| {
+                    rustls::Error::InvalidCertificateData(format!(
+                        "unable to parse certificate subject: {:?}",
+                        err
+                    ))
+                })?;
+
+            let common_name = subject.common_name.ok_or_else(|| {
+                rustls::Error::InvalidCertificateData(
+                    "certificate not valid for name, no SAN and no CN present".to_string(),
+                )
+            })?;
+
+            match common_name == server_name {
+                true => Ok(()),
+                false => Err(rustls::Error::InvalidCertificateData(
+                    "certificate not valid for name, no SAN and CN doesn't match".to_string(),
+                )),
+            }
+        }
+        Err(err) => Err(pki_error(err)), // Any other error means there was an error parsing the cert, we should throw
+    }
+}
+
+fn pki_error(error: webpki::Error) -> rustls::Error {
+    use webpki::Error::*;
+    match error {
+        BadDer | BadDerTime => rustls::Error::InvalidCertificateEncoding,
+        InvalidSignatureForPublicKey => rustls::Error::InvalidCertificateSignature,
+        UnsupportedSignatureAlgorithm | UnsupportedSignatureAlgorithmForPublicKey => {
+            rustls::Error::InvalidCertificateSignatureType
+        }
+        e => rustls::Error::InvalidCertificateData(format!("invalid peer certificate: {}", e)),
     }
 }
 
