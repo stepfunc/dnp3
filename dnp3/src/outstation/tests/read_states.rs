062d6c02db1c4c5695ca3d5f717fd3f8aebf9164
jadamcrain <jadamcrain@gmail.com>
2021-12-15 10:56:57 -0800

    add unit tests proving the issue exists

--- dnp3/src/outstation/tests/read_states.rs
+++ dnp3/src/outstation/tests/read_states.rs
@@ -6,6 +6,7 @@ use crate::outstation::database::*;
 use crate::outstation::tests::harness::*;
 
 const EMPTY_READ: &[u8] = &[0xC0, 0x01];
+const READ_CLASS_0: &[u8] = &[0xC0, 0x01, 60, 01, 0x06];
 const READ_CLASS_123: &[u8] = &[0xC0, 0x01, 60, 02, 0x06, 60, 03, 0x06, 60, 04, 0x06];
 const CONFIRM_SEQ_0: &[u8] = &[0xC0, 0x00];
 const UNS_CONFIRM_SEQ_0: &[u8] = &[0b11010000, 0x00];
@@ -30,6 +31,59 @@ fn empty_read_yields_empty_response() {
     let mut harness = new_harness(get_default_config());
     harness.test_request_response(EMPTY_READ, EMPTY_RESPONSE);
     harness.check_no_events();
+}
+
+#[test]
+fn can_read_one_byte_range() {
+    let mut harness = new_harness(get_default_config());
+
+    harness.handle.database.transaction(|database| {
+        database.add(0, Some(EventClass::Class1), AnalogConfig::default());
+        database.update(
+            0,
+            &Analog::new(42.0, Flags::ONLINE, Time::Synchronized(Timestamp::new(0))),
+            UpdateOptions::initialize(),
+        );
+    });
+
+    const READ_G30_V1_0_TO_0: &[u8] = &[0xC0, 0x01, 0x1E, 0x01, 0x00, 0x00, 0x00];
+    harness.test_request_response(
+        READ_G30_V1_0_TO_0,
+        &[
+            0xC0, 0x81, 0x80, 0x00, 0x1E, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
+            0x00, 0x00,
+        ],
+    );
+    harness.check_events(&[]);
+}
+
+#[test]
+fn can_read_two_byte_range() {
+    let mut harness = new_harness(get_default_config());
+
+    harness.handle.database.transaction(|database| {
+        for i in 0..5 {
+            database.add(i, Some(EventClass::Class1), BinaryConfig::default());
+            database.update(
+                i,
+                &Binary::new(
+                    i % 2 == 1,
+                    Flags::ONLINE,
+                    Time::Synchronized(Timestamp::new(0)),
+                ),
+                UpdateOptions::initialize(),
+            );
+        }
+    });
+
+    const READ_G1_V2_1_TO_3: &[u8] = &[0xC0, 0x01, 0x01, 0x02, 0x01, 0x01, 0x00, 0x03, 0x00];
+    harness.test_request_response(
+        READ_G1_V2_1_TO_3,
+        &[
+            0xC0, 0x81, 0x80, 0x00, 0x01, 0x02, 0x01, 0x01, 0x00, 0x03, 0x00, 0x81, 0x01, 0x81,
+        ],
+    );
+    harness.check_events(&[]);
 }
 
 #[test]
