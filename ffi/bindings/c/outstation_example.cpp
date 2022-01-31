39f300278f9256cb49142261557ae622faa1e477
jadamcrain <jadamcrain@gmail.com>
2022-01-31 11:27:29 -0800

    refactor outstation C++ example to include all transports

--- ffi/bindings/c/outstation_example.cpp
+++ ffi/bindings/c/outstation_example.cpp
@@ -129,35 +129,10 @@ dnp3::EventBufferConfig get_event_buffer_config()
     return EventBufferConfig(10, 10, 10, 10, 10, 10, 10, 10);
 }
 
-int main()
+void run_outstation(dnp3::Outstation &outstation)
 {
-    Logging::configure(LoggingConfig(), logger(
-        [](LogLevel level, std::string message) {
-            std::cout << message;
-        }
-    ));
-
-    auto runtime = Runtime(RuntimeConfig());
-    
-    TcpServer server(runtime, LinkErrorMode::close, "127.0.0.1:20000");
-
-    dnp3::OutstationConfig config(1024, 1);
-    config.decode_level.application = dnp3::AppDecodeLevel::object_values;
-
-    auto filter = AddressFilter::any();
-    auto outstation = server.add_outstation(
-        get_outstation_config(),
-        get_event_buffer_config(),
-        std::make_unique<MyOutstationApplication>(),
-        std::make_unique<MyOutstationInformation>(),
-        std::make_unique<MyControlHandler>(),
-        connection_state_listener([](ConnectionState state) {
-            std::cout << "ConnectionState: " << to_string(state) << std::endl;
-        }),
-        filter
-    );
-
-    auto setup = database_transaction([](Database& db) {
+    // setup the initial state of the outstation
+    auto setup = database_transaction([](Database &db) {
         // add 5 of each type
         for (uint16_t i = 0; i < 10; ++i) {
             db.add_binary_input(i, EventClass::class1, BinaryInputConfig());
@@ -172,27 +147,24 @@ int main()
     });
     outstation.transaction(setup);
 
-    server.bind();
-
     State state;
 
-    while (true)
-    {
+    while (true) {
         std::string cmd;
         std::getline(std::cin, cmd);
 
-        if(cmd == "x") {
-            return 0;
+        if (cmd == "x") {
+            return;
         }
         else if (cmd == "bi") {
-            auto modify = database_transaction([&](Database& db) {
+            auto modify = database_transaction([&](Database &db) {
                 state.binary = !state.binary;
                 db.update_binary_input(BinaryInput(7, state.binary, online(), now()), UpdateOptions());
             });
             outstation.transaction(modify);
-        }       
+        }
         else if (cmd == "dbbi") {
-            auto modify = database_transaction([&](Database& db) {
+            auto modify = database_transaction([&](Database &db) {
                 state.double_bit_binary = !state.double_bit_binary;
                 auto value = state.double_bit_binary ? DoubleBit::determined_on : DoubleBit::determined_off;
                 db.update_double_bit_binary_input(DoubleBitBinaryInput(3, value, online(), now()), UpdateOptions());
@@ -200,35 +172,35 @@ int main()
             outstation.transaction(modify);
         }
         else if (cmd == "bos") {
-            auto modify = database_transaction([&](Database& db) {
+            auto modify = database_transaction([&](Database &db) {
                 state.binary_output_status = !state.binary_output_status;
                 db.update_binary_output_status(BinaryOutputStatus(7, state.binary_output_status, online(), now()), UpdateOptions());
             });
             outstation.transaction(modify);
         }
         else if (cmd == "co") {
-            auto modify = database_transaction([&](Database& db) {
+            auto modify = database_transaction([&](Database &db) {
                 state.counter += 1;
                 db.update_counter(Counter(7, state.counter, online(), now()), UpdateOptions());
             });
             outstation.transaction(modify);
         }
         else if (cmd == "fco") {
-            auto modify = database_transaction([&](Database& db) {
+            auto modify = database_transaction([&](Database &db) {
                 state.frozen_counter += 1;
                 db.update_frozen_counter(FrozenCounter(7, state.frozen_counter, online(), now()), UpdateOptions());
             });
             outstation.transaction(modify);
         }
         else if (cmd == "ai") {
-            auto modify = database_transaction([&](Database& db) {
+            auto modify = database_transaction([&](Database &db) {
                 state.analog += 1;
                 db.update_analog_input(AnalogInput(7, state.analog, online(), now()), UpdateOptions());
             });
             outstation.transaction(modify);
         }
         else if (cmd == "aos") {
-            auto modify = database_transaction([&](Database& db) {
+            auto modify = database_transaction([&](Database &db) {
                 state.analog_output_status += 1;
                 db.update_analog_output_status(AnalogOutputStatus(7, state.analog_output_status, online(), now()), UpdateOptions());
             });
@@ -236,17 +208,126 @@ int main()
         }
         else if (cmd == "os") {
             std::vector<uint8_t> values;
-            for(auto x : std::string("hello world!")) {
+            for (auto x : std::string("hello world!")) {
                 values.push_back(x);
             }
 
-            auto modify = database_transaction([&](Database& db) {
-                db.update_octet_string(7, values, UpdateOptions());
-            });
+            auto modify = database_transaction([&](Database &db) { db.update_octet_string(7, values, UpdateOptions()); });
             outstation.transaction(modify);
         }
         else {
             std::cout << "unknown command: " << cmd << std::endl;
         }
     }
+}
+
+void run_server(dnp3::TcpServer &server)
+{
+    auto filter = AddressFilter::any();
+    auto outstation = server.add_outstation(
+        get_outstation_config(), get_event_buffer_config(), std::make_unique<MyOutstationApplication>(), std::make_unique<MyOutstationInformation>(),
+        std::make_unique<MyControlHandler>(),
+        connection_state_listener([](ConnectionState state) { std::cout << "ConnectionState: " << to_string(state) << std::endl; }), filter);
+
+    server.bind();
+
+    run_outstation(outstation);
+}
+
+void run_tcp_server(dnp3::Runtime &runtime)
+{
+    dnp3::TcpServer server(runtime, LinkErrorMode::close, "127.0.0.1:20000");
+    run_server(server);
+}
+
+void run_tls_server(dnp3::Runtime &runtime, const dnp3::TlsServerConfig &config)
+{
+    dnp3::TcpServer server = dnp3::TcpServer::create_tls_server(runtime, LinkErrorMode::close, "127.0.0.1:20000", config);
+    run_server(server);
+}
+
+void run_serial(dnp3::Runtime &runtime)
+{    
+    auto outstation = dnp3::Outstation::create_serial_session(
+        runtime,
+        "/dev/pts/4",
+        dnp3::SerialPortSettings(),
+        get_outstation_config(),
+        get_event_buffer_config(),
+        std::make_unique<MyOutstationApplication>(),
+        std::make_unique<MyOutstationInformation>(),
+        std::make_unique<MyControlHandler>()
+    );
+
+    run_outstation(outstation);
+}
+
+dnp3::TlsServerConfig get_tls_ca_config()
+{ 
+    // ANCHOR: tls_ca_chain_config
+    // defaults to CA mode
+    dnp3::TlsServerConfig config(
+        "test.com",
+        "./certs/ca_chain/ca_cert.pem",
+        "./certs/ca_chain/entity2_cert.pem", 
+        "./certs/ca_chain/entity2_key.pem",
+        "" // no password
+    );
+    // ANCHOR_END: tls_ca_chain_config
+
+    return config;
+}
+
+dnp3::TlsServerConfig get_tls_self_signed_config()
+{
+    // ANCHOR: tls_self_signed_config    
+    dnp3::TlsServerConfig config(
+        "test.com", 
+        "./certs/self_signed/entity1_cert.pem",
+        "./certs/self_signed/entity2_cert.pem",
+        "./certs/self_signed/entity2_key.pem",
+        "" // no password
+    );
+    config.certificate_mode = dnp3::CertificateMode::self_signed;
+    // ANCHOR_END: tls_self_signed_config
+
+    return config;
+}
+
+int main(int argc, char *argv[])
+{
+    Logging::configure(LoggingConfig(), logger(
+        [](LogLevel level, std::string message) {
+            std::cout << message;
+        }
+    ));
+
+    auto runtime = Runtime(RuntimeConfig());
+
+    if (argc != 2) {
+        std::cout << "you must specify a transport type" << std::endl;
+        std::cout << "usage: cpp-outstation-example <channel> (tcp, serial, tls-ca, tls-self-signed)" << std::endl;
+        return -1;
+    }
+
+    const auto type = argv[1];
+
+    if (strcmp(type, "tcp") == 0) {
+        run_tcp_server(runtime);
+    }
+    else if (strcmp(type, "serial") == 0) {
+        run_serial(runtime);
+    }
+    else if (strcmp(type, "tls-ca") == 0) {
+        run_tls_server(runtime, get_tls_ca_config());
+    }
+    else if (strcmp(type, "tls-self-signed") == 0) {
+        run_tls_server(runtime, get_tls_self_signed_config());
+    }
+    else {
+        std::cout << "unknown channel type: " << type << std::endl;
+        return -1;
+    }    
+
+    return 0;
 }
