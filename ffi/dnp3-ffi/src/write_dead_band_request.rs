0f852f8555d20584d185f80da5d3a30280df6106
jadamcrain <jadamcrain@gmail.com>
2022-12-19 12:02:47 -0800

    work on implementation for WriteDeadBandRequest

--- ffi/dnp3-ffi/src/write_dead_band_request.rs
+++ ffi/dnp3-ffi/src/write_dead_band_request.rs
@@ -1,11 +1,11 @@
 use dnp3::master::DeadBandHeader;
 
 pub struct WriteDeadBandRequest {
-    headers: Vec<Variant>,
-    current: Option<Variant>,
+    headers: Vec<Header>,
+    current: Option<Header>,
 }
 
-enum Variant {
+enum Header {
     /// Group 34 variation 1 with 8-bit index
     G34V1U8(Vec<(u16, u8)>),
     /// Group 34 variation 1 with 16-bit index
@@ -29,32 +29,32 @@ impl WriteDeadBandRequest {
         let mut ret: Vec<DeadBandHeader> = Default::default();
         for header in self.headers.iter() {
             match header {
-                Variant::G34V1U8(x) => {
+                Header::G34V1U8(x) => {
                     ret.push(DeadBandHeader::group34_var1_u8(
                         x.iter().map(|(i, v)| (*v, *i)).collect(),
                     ));
                 }
-                Variant::G34V1U16(x) => {
+                Header::G34V1U16(x) => {
                     ret.push(DeadBandHeader::group34_var1_u16(
                         x.iter().map(|(i, v)| (*v, *i)).collect(),
                     ));
                 }
-                Variant::G34V2U8(x) => {
+                Header::G34V2U8(x) => {
                     ret.push(DeadBandHeader::group34_var2_u8(
                         x.iter().map(|(i, v)| (*v, *i)).collect(),
                     ));
                 }
-                Variant::G34V2U16(x) => {
+                Header::G34V2U16(x) => {
                     ret.push(DeadBandHeader::group34_var2_u16(
                         x.iter().map(|(i, v)| (*v, *i)).collect(),
                     ));
                 }
-                Variant::G34V3U8(x) => {
+                Header::G34V3U8(x) => {
                     ret.push(DeadBandHeader::group34_var3_u8(
                         x.iter().map(|(i, v)| (*v, *i)).collect(),
                     ));
                 }
-                Variant::G34V3U16(x) => {
+                Header::G34V3U16(x) => {
                     ret.push(DeadBandHeader::group34_var3_u16(
                         x.iter().map(|(i, v)| (*v, *i)).collect(),
                     ));
@@ -76,5 +76,57 @@ pub(crate) fn write_dead_band_request_create() -> *mut crate::WriteDeadBandReque
 pub(crate) unsafe fn write_dead_band_request_destroy(instance: *mut crate::WriteDeadBandRequest) {
     if !instance.is_null() {
         drop(Box::from_raw(instance));
+    }
+}
+
+pub(crate) unsafe fn write_dead_band_request_add_g32v1_u8(
+    instance: *mut crate::WriteDeadBandRequest,
+    index: u8,
+    dead_band: u16,
+) {
+    write_dead_band_request_add_generic(instance, (dead_band, index))
+}
+
+unsafe fn write_dead_band_request_add_generic<T>(
+    instance: *mut crate::WriteDeadBandRequest,
+    item: T,
+) where
+    T: DeadBandVariant,
+{
+    let instance = match instance.as_mut() {
+        None => return,
+        Some(x) => x,
+    };
+
+    match instance.current.take() {
+        None => instance.current = Some(T::wrap(item)),
+        Some(v) => match T::extend(v, item) {
+            Ok(x) => instance.current = Some(x),
+            Err((old, new)) => {
+                instance.headers.push(old);
+                instance.current = Some(new);
+            }
+        },
+    }
+}
+
+trait DeadBandVariant {
+    fn wrap(item: Self) -> Header;
+    fn extend(header: Header, item: Self) -> Result<Header, (Header, Header)>;
+}
+
+impl DeadBandVariant for (u16, u8) {
+    fn wrap(item: Self) -> Header {
+        Header::G34V1U8(vec![item])
+    }
+
+    fn extend(header: Header, item: Self) -> Result<Header, (Header, Header)> {
+        match header {
+            Header::G34V1U8(mut x) => {
+                x.push(item);
+                Ok(Header::G34V1U8(x))
+            }
+            _ => Err((header, Self::wrap(item))),
+        }
     }
 }
--- ffi/dnp3-schema/src/write_dead_band_request.rs
+++ ffi/dnp3-schema/src/write_dead_band_request.rs
@@ -13,10 +13,22 @@ pub fn define(lib: &mut LibraryBuilder) -> BackTraced<ClassHandle> {
         "Destroy a request created with {class:write_dead_band_request.[constructor]}",
     )?;
 
+    let add_g32v1_u8 = lib
+        .define_method("add_g32v1_u8", request.clone())?
+        .doc("Add a GV34Var1 16-bit dead-band with 8-bit indexing  to the request")?
+        .param(
+            "index",
+            Primitive::U8,
+            "Index of the analog input to which the dead-band applies",
+        )?
+        .param("dead_band", Primitive::U16, "Value of the dead-band")?
+        .build()?;
+
     let request = lib
         .define_class(&request)?
         .constructor(constructor)?
         .destructor(destructor)?
+        .method(add_g32v1_u8)?
         .doc(doc(
             "Define a custom request to WRITE analog input dead-bands",
         ))?
