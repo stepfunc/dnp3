7a401689d2f86a06412a72bcd72a323f41c4488d
jadamcrain <jadamcrain@gmail.com>
2022-12-21 11:59:27 -0800

    add tests for pkcs8 key decryption

--- dnp3/src/tcp/tls/mod.rs
+++ dnp3/src/tcp/tls/mod.rs
@@ -196,7 +196,6 @@ fn load_certs(path: &Path, is_local: bool) -> Result<Vec<rustls::Certificate>, T
 }
 
 fn load_private_key(path: &Path, password: Option<&str>) -> Result<rustls::PrivateKey, TlsError> {
-
     let expected_tag = match &password {
         Some(_) => "ENCRYPTED PRIVATE KEY",
         None => "PRIVATE KEY",
@@ -214,9 +213,8 @@ fn load_private_key(path: &Path, password: Option<&str>) -> Result<rustls::Priva
     let key = match iter.next() {
         Some(key) => match password {
             Some(password) => {
-                let encrypted = pkcs8::EncryptedPrivateKeyInfo::try_from(key.as_slice())?;
-                let decrypted = encrypted.decrypt(password)?;
-                rustls::PrivateKey(decrypted.as_bytes().to_owned())
+                let key = DecryptedKey::parse_and_decrypt(&key, password)?;
+                rustls::PrivateKey(key.as_bytes().to_owned())
             }
             None => rustls::PrivateKey(key),
         },
@@ -239,6 +237,23 @@ fn load_private_key(path: &Path, password: Option<&str>) -> Result<rustls::Priva
     Ok(key)
 }
 
+struct DecryptedKey {
+    inner: pkcs8::SecretDocument,
+}
+
+impl DecryptedKey {
+    fn parse_and_decrypt(der: &[u8], password: &str) -> Result<Self, TlsError> {
+        let parsed = pkcs8::EncryptedPrivateKeyInfo::try_from(der)?;
+        Ok(Self {
+            inner: parsed.decrypt(password)?,
+        })
+    }
+
+    fn as_bytes(&self) -> &[u8] {
+        self.inner.as_bytes()
+    }
+}
+
 impl From<pkcs8::der::Error> for TlsError {
     fn from(from: pkcs8::der::Error) -> Self {
         TlsError::InvalidPrivateKey(io::Error::new(ErrorKind::InvalidData, from.to_string()))
@@ -248,5 +263,85 @@ impl From<pkcs8::der::Error> for TlsError {
 impl From<pkcs8::Error> for TlsError {
     fn from(from: pkcs8::Error) -> Self {
         TlsError::InvalidPrivateKey(io::Error::new(ErrorKind::InvalidData, from.to_string()))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::tcp::tls::DecryptedKey;
+
+    const TEST_KEY: &str = r#"
+-----BEGIN ENCRYPTED PRIVATE KEY-----
+MIIFJTBPBgkqhkiG9w0BBQ0wQjAhBgkrBgEEAdpHBAswFAQIu0Ufk06Ty+ICAkAA
+AgEIAgEBMB0GCWCGSAFlAwQBKgQQIP2MoQA/IuQ9YgoLJAHEnQSCBNBMF6XHpHn8
+lKR0MfyeCPi1bGgpp39c6s3he9WdB57Z9r9SLrACbdMeLzOfbr5hF2JmCYk0T7Us
+p6s20q5tiwd9zDWAHbGKOnzpVlLJhhz4GvfaTVt6K0onPt7Y3mfB9P44G3p3j83Z
+3Ekg784DH26gYgIYK8uo0PNnBZbuoVTiBdj2BtsJpBysoztPeCEbF6xjw4p8obEo
+YYOH2djLOfWipr0iqIdX6IAPQ/zKjAkZHy1VwEYcSmE5YS6UbzUkzFGEt4tV9RYJ
+4ctJO2PVpFvmdmVvYCrzJI6BHX0a1AYd5FV/j+2Hd/wnRhp2srLv/rsYHF/J6VK0
+E6FuLjLDHcG4TthQSlGJ/ewT6xiSZ7HdDn3BJfOJR9d9V8f+FfpEVL55lzPOh9Fl
+Ad8cNClliziJCgkAJ7MhUenNwTnxOQYrnVDDUMTNYEiK6EoUWlSDbLp13n/uhmaw
+huXvfLAMSmfJvWAEcnXn7X+/mHkEMWtrZkvHg2yDfyTk/8ZtRIw25+hp6MOBpbtn
+7py2QlefjDMa5wAQGqACVNkMFUzEpNBNh1hj3CHBYdE1Jo5dyZPFcuGWY8ye75RI
+F3Lq/z3NqbaebPUmXlpLh0YSvpyoZJM+Knr6bCGWj1Ik3oq0+gqbGmPQKULpmdkz
+5JgD0TsD/yQL6Ldm1KMzhJmwovH25YIxcrbGmlQ2658XGNS/3kPR0UbdIgR4unWz
+XcwV75HKT4Rc0E0fAYKzFVE9J08aQawhNKbaGGc8zNQiz9tGSVvpO3OLf35tHoPz
+eRs4flWGR/b/seGeEcVzeO5RDNDxXblfoSc/gB7lPdA35ig9z6egQvUyJrmIUhGa
+lRKg9UeXsT2gZZHZPCaYekWacBtNWYKzdrdgSHxxKkjvF/tWKxE62RaWzuwqs0qK
+tEa7RBKwe+wYRp4KVWe15XO5dvfYYtGkza70QYhQw86foAtYHpI6nMv4ppBf/vkD
+mcivoGnImRMGlt1Klo1I3VjKI3lvms9UpmNuI35THwxnz5O1aCvS1pBvXEs+D95y
+4qvhVkcbYMF3anxgn0ZD+3LYTxWuPbRBxh6GxC4qbn9tPHtN/7Iop7pSqaQMBFt2
+1pe/PuGpiswQY3mtU3WLVP4pC0Mu5KDShswRQmI7XLOIjQgT5ac3JrFogqgkX+rb
+9ZS7jHDBNY7eGsI7sLiMVRnHltiOOwhBHu9+NAwi1jmJIvSPLJzf/MW6rRnpg10D
+pHC/LdUlon31MBb5kqpidhQa1LD9gWzesMLq3DMkUbTAbY0sddSP8XkUidLsZEMx
+mjmaNlfKhLNE0N7o59t40+l3W2bghnsd/VC9fZL66ShSISe3bzHjnJklpLNHhl3/
+gFwoMvSG8u6Aboz5QfXFS5HliIf5Fnw8ed+deEb5z3fiSpKi0PbcVRzQWotzG152
+FKueCGIlqyTzlh7j8wY8tnChbY/34kIGFUDtKQcAjR6mLSi80s53dBG/lIZAEsG5
+P1x3xO3fzO5w9X5yrRxGnN6N6McMqTkF1V4DhUfwrb41QBR2UBdbed126ZhM52fc
+9We9YmUyRcV38yXPJ6SB6I9xdNIa5Nv4tB/7DzvpaRS6FBUPVm+VsULxc2XIjHGp
+LOCHvWcZO49jGhZErBtxk16H58koZVg5Zw==
+-----END ENCRYPTED PRIVATE KEY-----"#;
+
+    const DECYPTED_KEY: &str = r#"
+-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDOX/fVrsEE5ihY
+BIi97q2u1j7lMx+3plyDBOzjK7uovVtmGadtmfLxtRvrFYG4tdUNZ6svhiFvutNW
+GQxT4RWw//UkhnS3Znh+VhR0C4qsrMARP3svugFqLkwqS36jWLAy3bvVXlkxsKKi
+Tjf5DQwrgM2s6s7QBYMJNN8+jBsYlZXf+5bUN4pFk6vSU7NFPZUSBZ/u3rSa4mR6
+aD0QRMEYW4+fjj7+g+aZgUHHQT8Cz1FKCAbHSgVugOVC7Pwn3L9ozH2RC/cQdDhD
+Arq+P1o7+4YyYG12bp/D5lFu+kdT6p0mHSx6l11sEAEIUGGeI/685K3GwZ4t2ip0
+qQ+/xbVdAgMBAAECggEBAJjq8WgrWii4JjK6AVzDK3z+kZIhpKHfKnOGxcS6lg29
+aako3y/OP/8r1KkHwZxNV7XcGDNZrxLsG0aTvte0U+9YaZwL6RYwXp42SGeIWdQD
+GTpukGfX6s5zycoZMJf20nCObmz2wR6ZpJihXsYzDc56XWyAfIgVXXgH7leZV0aJ
+08pEv0kAHGZSrxHGBT3n6+wrkHduTZ89Wx3kubwJRiRtHp8IdNhNoxlN44e5uizu
+7ix6k/pg5TDaXOwsp4cE8SQ3zUa+kvazX693sCZdhCKlmrwYP9WZCAWLAy8sRFM+
+SAj2cNWva1LL/hE3pgrCg4BV9zmr0UFMSLJxdcDAQZ0CgYEA69qUzzWU8KIKmXf5
+J7yWKbUftXwIn4DrlabrpeWCYsvxfNsZWYEc+8VqZDczwwI2nf9useXQhfxH7VSc
+q5d20ApNY3hHemp6s1p9224oj/6rKJtPBOdSIyAs1cOjNYK+YXcVjYlWHvHBpraL
+ct2vo8yTkPkLAOJEqckPbadkVFsCgYEA4ADFJACr1uDK2aMqjR2fEz2GBm+om7j0
+Omj6STyHuYxb4wlUzC58sqtNVXGTVB+ha1HZT+DM7NHkCtEQLN2dqVrFCgv39MzI
+huxD59j0hsVrqnK23JswgsiJar8AUiEe2MY/ysgHVuUtkDzKEN+bgcVhqap0sn4C
+q19oOcA/aqcCgYAefhcJJxtHdRu7tbgfvBEJ+WHNG+kdfhR3N6p1u1N9JHLnOohv
+evLdViuoIz7s8mdPTAvqshSgjfpao7rRsHZq9ToGJzHOkN+mOofVC8vwufM0/8da
+kfGbmvhQ9scuDuZAQZ4mu1/IBmeL/0POKP0hRzy43IngpmBMNzNocODWywKBgC/b
+ukLw6cXlDTHmjIbN11jTAjmJzapHn9aC60aOaikYdeFR8w4UuIur0b/5nhKRF3nI
+aPeJ/f5y8ZfmBuCvEKpIPGTjHbztq8I35GI6ljPdJh2qmKsVdQ3cLo/h8v2ZGfAS
+mzqF9ht4p31zn3BvddgKBc2sH3arOYLHxYrhKittAoGAIpHK6rYu3O/1eF5Nimok
+fRpT+FVOxgRX9fBL6IBuYzZPvtDfhq1kgzOYBo1oW+ObaF9BnFvYdE2CEX8Gr0+Z
+s6psX64trGW6DcgvM8bQtQa1EfRSp/EifPGwa5tzPw0UVF/VdpFa9Lum7cAjTDsm
+jKIQUja2I9E99ZWstIdBCUE=
+-----END PRIVATE KEY-----"#;
+
+    #[test]
+    fn can_decrypt_pkcs8_key() {
+        let key = {
+            let key = pem::parse(TEST_KEY).unwrap();
+            assert_eq!(key.tag, "ENCRYPTED PRIVATE KEY");
+            DecryptedKey::parse_and_decrypt(&key.contents, "foobar").unwrap()
+        };
+
+        let decrypted = pem::parse(DECYPTED_KEY).unwrap();
+        assert_eq!(decrypted.tag, "PRIVATE KEY");
+        assert_eq!(key.as_bytes(), decrypted.contents);
     }
 }
