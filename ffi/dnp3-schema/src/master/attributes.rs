0fdccde498723c9d1915414f2abbe5c576e2b67e
jadamcrain <jadamcrain@gmail.com>
2023-01-04 12:29:54 -0800

    Define all FFI attr enums from const list

--- ffi/dnp3-schema/src/master/attributes.rs
+++ ffi/dnp3-schema/src/master/attributes.rs
@@ -1,5 +1,6 @@
 use oo_bindgen::model::*;
 
+#[derive(PartialEq, Eq)]
 enum KnownAttributeType {
     List,
     String,
@@ -51,7 +52,12 @@ const fn all(variation: u8, name: &'static str, desc: &'static str) -> KnownAttr
 }
 
 impl KnownAttribute {
-    const fn new(variation: u8, name: &'static str, desc: &'static str, attr_type: KnownAttributeType) -> Self {
+    const fn new(
+        variation: u8,
+        name: &'static str,
+        desc: &'static str,
+        attr_type: KnownAttributeType,
+    ) -> Self {
         Self {
             variation,
             name,
@@ -75,7 +81,7 @@ const ATTRIBUTES: &[KnownAttribute] = &[
     string(206, "user_assigned_secondary_operator_name", "User-assigned secondary operator name"),
     string(207, "user_assigned_primary_operator_name", "User-assigned primary operator name"),
     string(208,"user_assigned_system_name", "User-assigned system name"),
-    string(209, "secure_auth_version", "Secure authentication version"),
+    uint(209, "secure_auth_version", "Secure authentication version"),
     uint(210, "num_security_stats_per_assoc", "Number of security statistics per association"),
     string(211, "user_specific_attributes", "Identification of user-specific attributes"),
     uint(212, "num_master_defined_data_set_proto", "Number of master defined data-set prototypes"),
@@ -122,8 +128,6 @@ const ATTRIBUTES: &[KnownAttribute] = &[
     list(255, "list_of_variations", "List of attribute variations"),
 ];
 
-
-
 pub(crate) struct DeviceAttrTypes {
     pub(crate) variation_list_attr: EnumHandle,
     pub(crate) string_attr: EnumHandle,
@@ -153,10 +157,16 @@ pub(crate) fn define(lib: &mut LibraryBuilder) -> BackTraced<DeviceAttrTypes> {
 }
 
 fn define_attr_constants(lib: &mut LibraryBuilder) -> BackTraced<()> {
-    let mut builder = lib.define_constants("attribute_variations")?.doc("Device attribute variation constants")?;
+    let mut builder = lib
+        .define_constants("attribute_variations")?
+        .doc("Device attribute variation constants")?;
 
     for attr in ATTRIBUTES {
-        builder = builder.add(attr.name, ConstantValue::U8(attr.variation, Representation::Hex), attr.desc)?;
+        builder = builder.add(
+            attr.name,
+            ConstantValue::U8(attr.variation, Representation::Hex),
+            attr.desc,
+        )?;
     }
 
     builder.build()?;
@@ -216,193 +226,51 @@ impl<'a> UnknownCase for EnumBuilder<'a> {
     }
 }
 
-fn define_variation_list_attr(lib: &mut LibraryBuilder) -> BackTraced<EnumHandle> {
-    let value = lib
-        .define_enum("variation_list_attr")?
-        .doc("Enumeration of all the variation list attributes")?
-        .push(
-            "list_of_variations",
-            "Variation 255  - List of attribute variations",
-        )?
-        .add_unknown()?
-        .build()?;
+fn define_attr_enum(
+    lib: &mut LibraryBuilder,
+    typ: KnownAttributeType,
+    name: &str,
+    doc: Doc<Unvalidated>,
+) -> BackTraced<EnumHandle> {
+    let mut builder = lib.define_enum(name)?.doc(doc)?.add_unknown()?;
+
+    for attr in ATTRIBUTES.iter().filter(|x| x.attr_type == typ) {
+        builder = builder.push(
+            attr.name,
+            &format!("Variation {} - {}", attr.variation, attr.desc),
+        )?;
+    }
+
+    let value = builder.build()?;
 
     Ok(value)
+}
+
+fn define_variation_list_attr(lib: &mut LibraryBuilder) -> BackTraced<EnumHandle> {
+    define_attr_enum(
+        lib,
+        KnownAttributeType::List,
+        "variation_list_attr",
+        doc("Enumeration of all the variation list attributes"),
+    )
 }
 
 fn define_string_attr(lib: &mut LibraryBuilder) -> BackTraced<EnumHandle> {
-    let value = lib
-        .define_enum("string_attr")?
-        .doc("Enumeration of all the default string attributes")?
-        .push("config_id", "Variation 196 - Configuration id")?
-        .push("config_version", "Variation 197 - Configuration version")?
-        .push(
-            "config_digest_algorithm",
-            "Variation 201 - Configuration digest algorithm",
-        )?
-        .push(
-            "master_resource_id",
-            "Variation 202 - Master resource id (mRID)",
-        )?
-        .push(
-            "user_assigned_secondary_operator_name",
-            "Variation 206 - User-assigned secondary operator name",
-        )?
-        .push(
-            "user_assigned_primary_operator_name",
-            "Variation 207 - User-assigned primary operator name",
-        )?
-        .push(
-            "user_assigned_system_name",
-            "Variation 208 - User-assigned system name",
-        )?
-        .push(
-            "user_specific_attributes",
-            "Variation 211 - Identification of user-specific attributes",
-        )?
-        .push(
-            "device_manufacturer_software_version",
-            "Variation 242 - Device manufacturer software version",
-        )?
-        .push(
-            "device_manufacturer_hardware_version",
-            "Variation 243 - Device manufacturer hardware version",
-        )?
-        .push(
-            "user_assigned_owner_name",
-            "Variation 244 - User-assigned owner name",
-        )?
-        .push(
-            "user_assigned_location",
-            "Variation 245 - User assigned location/name",
-        )?
-        .push(
-            "user_assigned_id",
-            "Variation 246 - User assigned ID code/number",
-        )?
-        .push(
-            "user_assigned_device_name",
-            "Variation 247 - User assigned device name",
-        )?
-        .push(
-            "device_serial_number",
-            "Variation 248 - Device serial number",
-        )?
-        .push(
-            "device_subset_and_conformance",
-            "Variation 249 - DNP3 subset and conformance",
-        )?
-        .push(
-            "product_name_and_model",
-            "Variation 250 - Device manufacturer's product name and model",
-        )?
-        .push(
-            "device_manufacturers_name",
-            "Variation 252 - Device manufacturer's name",
-        )?
-        .add_unknown()?
-        .build()?;
-
-    Ok(value)
+    define_attr_enum(
+        lib,
+        KnownAttributeType::String,
+        "string_attr",
+        doc("Enumeration of all the default string attributes"),
+    )
 }
 
 fn define_uint_attr(lib: &mut LibraryBuilder) -> BackTraced<EnumHandle> {
-    let value = lib
-        .define_enum("uint_attr")?
-        .doc("Enumeration of all the default string attributes")?
-        .push(
-            "secure_auth_version",
-            "Variation 209 - Secure authentication version",
-        )?
-        .push(
-            "num_security_stats_per_assoc",
-            "Variation 210 - Number of security statistics per association",
-        )?
-        .push(
-            "num_master_defined_data_set_proto",
-            "Variation 212 - Number of master defined data-set prototypes",
-        )?
-        .push(
-            "num_outstation_defined_data_set_proto",
-            "Number of outstation defined data-set prototypes",
-        )?
-        .push(
-            "num_master_defined_data_sets",
-            "Variation 214 - Number of master defined data-sets",
-        )?
-        .push(
-            "num_outstation_defined_data_sets",
-            "Variation 215 - Number of outstation defined data-sets",
-        )?
-        .push(
-            "max_binary_output_per_request",
-            "Variation 216 - Maximum number of binary outputs per request",
-        )?
-        .push(
-            "local_timing_accuracy",
-            "Variation 217 - Local timing accuracy (microseconds)",
-        )?
-        .push(
-            "duration_of_time_accuracy",
-            "Variation 218 - Duration of time accuracy (seconds)",
-        )?
-        .push(
-            "max_analog_output_index",
-            "Variation 220 - Maximum analog output index",
-        )?
-        .push(
-            "num_analog_outputs",
-            "Variation 221 - Number of analog outputs",
-        )?
-        .push(
-            "max_binary_output_index",
-            "Variation 223 - Maximum binary output index",
-        )?
-        .push(
-            "num_binary_outputs",
-            "Variation 224 - Number of binary outputs",
-        )?
-        .push(
-            "max_counter_index",
-            "Variation 228 - Maximum counter point index",
-        )?
-        .push("num_counter", "Variation 229 - Number of counter points")?
-        .push(
-            "max_analog_input_index",
-            "Variation 232 - Maximum analog input point index",
-        )?
-        .push(
-            "num_analog_input",
-            "Variation 233 - Number of analog input points",
-        )?
-        .push(
-            "max_double_bit_binary_input_index",
-            "Variation 235 - Maximum double-bit binary input point index",
-        )?
-        .push(
-            "num_double_bit_binary_input",
-            "Variation 236 - Number of double-bit binary input points",
-        )?
-        .push(
-            "max_binary_input_index",
-            "Variation 238 - Maximum binary input point index",
-        )?
-        .push(
-            "num_binary_input",
-            "Variation 239 - Number of binary input points",
-        )?
-        .push(
-            "max_tx_fragment_size",
-            "Variation 240 - Maximum transmit fragment size",
-        )?
-        .push(
-            "max_rx_fragment_size",
-            "Variation 241 - Maximum receive fragment size",
-        )?
-        .add_unknown()?
-        .build()?;
-
-    Ok(value)
+    define_attr_enum(
+        lib,
+        KnownAttributeType::UInt,
+        "uint_attr",
+        doc("Enumeration of all the default uint attributes"),
+    )
 }
 
 fn define_int_attr(lib: &mut LibraryBuilder) -> BackTraced<EnumHandle> {
@@ -419,62 +287,32 @@ fn define_int_attr(lib: &mut LibraryBuilder) -> BackTraced<EnumHandle> {
 }
 
 fn define_bool_attr(lib: &mut LibraryBuilder) -> BackTraced<EnumHandle> {
-    let value = lib.define_enum("bool_attr")?
-        .doc(doc("Enumeration of all the known boolean attributes").details("Boolean attributes are actually just encoded as signed integer attributes where 1 == true"))?
-        .push("supports_analog_output_events", "Variation 219 - Supports analog output events")?
-        .push("supports_binary_output_events", "Variation 222 - Supports binary output events")?
-        .push("supports_frozen_counter_events", "Variation 225 - Supports frozen counter events")?
-        .push("supports_frozen_counters", "Variation 226 - Supports frozen counters")?
-        .push("supports_counter_events", "Variation 227 - Supports counter events")?
-        .push("supports_frozen_analog_inputs", "Variation 230 - Support frozen analog input events")?
-        .push("supports_analog_input_events","Variation 231 - Support analog input events")?
-        .push("supports_double_bit_binary_input_events", "Variation 234 - Support double-bit binary input events")?
-        .push("supports_binary_input_events","Variation 237 - Support binary input events")?
-        .add_unknown()?
-        .build()?;
-
-    Ok(value)
+    define_attr_enum(lib, KnownAttributeType::Bool, "bool_attr",doc("Enumeration of all the known boolean attributes").details("Boolean attributes are actually just encoded as signed integer attributes where 1 == true"))
 }
 
 fn define_time_attr(lib: &mut LibraryBuilder) -> BackTraced<EnumHandle> {
-    let value = lib.define_enum("time_attr")?
-        .doc("Enumeration of all the known DNP3 Time attributes")?
-        .push("config_build_date", "Variation 198 - Time and date that the outstation's current configuration was built defined")?
-        .push("config_last_change_date", "Variation 199 - Time and date that the outstation's configuration was last modified")?
-        .add_unknown()?
-        .build()?;
-
-    Ok(value)
+    define_attr_enum(
+        lib,
+        KnownAttributeType::Time,
+        "time_attr",
+        doc("Enumeration of all the known DNP3 Time attributes"),
+    )
 }
 
 fn define_octet_string_attr(lib: &mut LibraryBuilder) -> BackTraced<EnumHandle> {
-    let value = lib.define_enum("octet_string_attr")?
-        .doc("Enumeration of all known octet-string attributes")?
-        .push("config_digest", "Variation 200 - Digest (aka fingerprint) of the configuration using a CRC, HASH, MAC, or public key signature")?
-        .add_unknown()?
-        .build()?;
-
-    Ok(value)
+    define_attr_enum(
+        lib,
+        KnownAttributeType::OctetString,
+        "octet_string_attr",
+        doc("Enumeration of all known octet-string attributes"),
+    )
 }
 
 fn define_float_attr(lib: &mut LibraryBuilder) -> BackTraced<EnumHandle> {
-    let value = lib
-        .define_enum("float_attr")?
-        .doc("Enumeration of all known float attributes")?
-        .push(
-            "device_location_altitude",
-            "Variation 203 - Altitude of the device",
-        )?
-        .push(
-            "device_location_longitude",
-            "Variation 204 - Longitude of the device from reference meridian (-180.0 to 180.0 deg)",
-        )?
-        .push(
-            "device_location_latitude",
-            "Variation 205 - Latitude of the device from the equator (90.0 to -90.0 deg)",
-        )?
-        .add_unknown()?
-        .build()?;
-
-    Ok(value)
+    define_attr_enum(
+        lib,
+        KnownAttributeType::Float,
+        "float_attr",
+        doc("Enumeration of all known float attributes"),
+    )
 }
