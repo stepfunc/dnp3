ee4c66f55fca616ea4aaad569425a2c02f495202
jadamcrain <jadamcrain@gmail.com>
2023-01-30 08:18:37 -0800

    first pass at handling file read response data

--- dnp3/src/master/tasks/file_read.rs
+++ dnp3/src/master/tasks/file_read.rs
@@ -1,6 +1,8 @@
 use crate::app::file::*;
 use crate::app::format::write::HeaderWriter;
 use crate::app::format::WriteError;
+use crate::app::parse::free_format::FreeFormatVariation;
+use crate::app::parse::parser::{HeaderDetails, ObjectHeader, Response};
 use crate::app::{FunctionCode, Timestamp};
 
 pub(crate) struct AuthData {
@@ -17,9 +19,48 @@ struct FileHandle(u32);
 #[derive(Copy, Clone, Default)]
 struct BlockNumber(u32);
 
+impl BlockNumber {
+    const LAST_BIT: u32 = 0x80_00_00_00;
+    const BOTTOM_BITS: u32 = !Self::LAST_BIT;
+
+    fn is_last(self) -> bool {
+        (self.0 & Self::LAST_BIT) != 0
+    }
+
+    fn bottom_bits(self) -> u32 {
+        self.0 & Self::BOTTOM_BITS
+    }
+}
+
+impl PartialEq for BlockNumber {
+    fn eq(&self, other: &Self) -> bool {
+        let b1 = self.0 & Self::BOTTOM_BITS;
+        let b2 = other.0 & Self::BOTTOM_BITS;
+        b1 == b2
+    }
+}
+
 pub(crate) struct Settings {
     pub(crate) name: Filename,
     pub(crate) max_block_size: u16,
+    pub(crate) max_file_size: usize,
+}
+
+#[derive(Copy, Clone)]
+struct ReadState {
+    handle: FileHandle,
+    block: BlockNumber,
+    num_bytes_rx: usize,
+}
+
+impl ReadState {
+    fn new(handle: FileHandle) -> Self {
+        Self {
+            handle,
+            block: Default::default(),
+            num_bytes_rx: 0,
+        }
+    }
 }
 
 /// States of the file transfer
@@ -29,7 +70,7 @@ enum State {
     /// Open the file - We might state in this state w/ the default AuthKey if auth not required
     Open(AuthKey),
     /// Read the next block
-    Read(FileHandle, BlockNumber),
+    Read(ReadState),
     /// Close the file
     Close(FileHandle),
 }
@@ -42,25 +83,23 @@ pub(crate) struct FileReadTask {
 }
 
 impl FileReadTask {
+    fn new(settings: Settings, state: State) -> Self {
+        Self { settings, state }
+    }
+
     pub(crate) fn auth(settings: Settings, auth: AuthData) -> Self {
-        Self {
-            settings,
-            state: State::GetAuth(auth),
-        }
+        Self::new(settings, State::GetAuth(auth))
     }
 
     pub(crate) fn open(settings: Settings) -> Self {
-        Self {
-            settings,
-            state: State::Open(AuthKey::default()),
-        }
+        Self::new(settings, State::Open(AuthKey::default()))
     }
 
     pub(crate) fn function(&self) -> FunctionCode {
         match self.state {
             State::GetAuth(_) => FunctionCode::AuthenticateFile,
             State::Open(_) => FunctionCode::OpenFile,
-            State::Read(_, _) => FunctionCode::Read,
+            State::Read(_) => FunctionCode::Read,
             State::Close(_) => FunctionCode::CloseFile,
         }
     }
@@ -69,9 +108,160 @@ impl FileReadTask {
         match &self.state {
             State::GetAuth(auth) => write_auth(auth, writer),
             State::Open(auth) => write_open(&self.settings, *auth, writer),
-            State::Read(handle, block) => write_read(*handle, *block, writer),
+            State::Read(rs) => write_read(*rs, writer),
             State::Close(handle) => write_close(*handle, writer),
         }
+    }
+
+    pub(crate) fn handle(self, response: Response) -> Option<FileReadTask> {
+        let headers = match response.objects {
+            Ok(x) => x,
+            Err(err) => {
+                tracing::warn!("File operation received malformed response: {err}");
+                return None;
+            }
+        };
+
+        let header = match headers.get_only_header() {
+            None => {
+                tracing::warn!("File operation response contains unexpected number of headers");
+                return None;
+            }
+            Some(x) => x,
+        };
+
+        match self.state {
+            State::GetAuth(_) => Self::handle_auth_response(self.settings, header),
+            State::Open(_) => Self::handle_open_response(self.settings, header),
+            State::Read(rs) => Self::handle_read_response(self.settings, rs, header),
+            State::Close(_) => Self::handle_close_response(header),
+        }
+    }
+
+    fn handle_auth_response(settings: Settings, header: ObjectHeader) -> Option<FileReadTask> {
+        let obj = match header.details {
+            HeaderDetails::TwoByteFreeFormat(_, FreeFormatVariation::Group70Var2(obj)) => obj,
+            _ => {
+                tracing::warn!(
+                    "File AUTHENTICATE response contains unexpected variation: {}",
+                    header.variation
+                );
+                return None;
+            }
+        };
+
+        if obj.auth_key == 0 {
+            tracing::warn!("Outstation returned auth key == 0: no permission to access file");
+            None
+        } else {
+            Some(FileReadTask::new(
+                settings,
+                State::Open(AuthKey(obj.auth_key)),
+            ))
+        }
+    }
+
+    fn handle_open_response(settings: Settings, header: ObjectHeader) -> Option<FileReadTask> {
+        let obj = match header.details {
+            HeaderDetails::TwoByteFreeFormat(_, FreeFormatVariation::Group70Var4(obj)) => obj,
+            _ => {
+                tracing::warn!(
+                    "File OPEN response contains unexpected variation: {}",
+                    header.variation
+                );
+                return None;
+            }
+        };
+
+        if obj.status_code != FileStatus::Success {
+            tracing::warn!("Unable to open file (status code == {:?})", obj.status_code);
+            return None;
+        }
+
+        let next = State::Read(ReadState::new(FileHandle(obj.file_handle)));
+
+        Some(FileReadTask::new(settings, next))
+    }
+
+    fn handle_read_response(
+        settings: Settings,
+        rs: ReadState,
+        header: ObjectHeader,
+    ) -> Option<FileReadTask> {
+        let obj = match header.details {
+            HeaderDetails::TwoByteFreeFormat(_, FreeFormatVariation::Group70Var5(obj)) => obj,
+            _ => {
+                tracing::warn!(
+                    "File READ response contains unexpected variation: {}",
+                    header.variation
+                );
+                return None;
+            }
+        };
+
+        let rx_block = BlockNumber(obj.block_number);
+
+        if rx_block.bottom_bits() != rs.block.bottom_bits() {
+            tracing::warn!(
+                "Expected file block {} but received block {}",
+                rs.block.bottom_bits(),
+                rx_block.bottom_bits()
+            );
+            return None;
+        }
+
+        let new_total = match rs.num_bytes_rx.checked_add(obj.file_data.len()) {
+            None => {
+                tracing::error!("File transfer overflow");
+                return None;
+            }
+            Some(x) => {
+                tracing::info!("Received {} of total {}", obj.file_data.len(), x);
+                x
+            }
+        };
+
+        if new_total > settings.max_file_size {
+            tracing::info!(
+                "Exceeded file size transfer limit of {}. Aborting file transfer.",
+                settings.max_file_size
+            );
+            return None;
+        }
+
+        let next = if rx_block.is_last() {
+            State::Close(rs.handle)
+        } else {
+            State::Read(ReadState {
+                handle: rs.handle,
+                block: BlockNumber(obj.block_number + 1),
+                num_bytes_rx: new_total,
+            })
+        };
+
+        Some(FileReadTask::new(settings, next))
+    }
+
+    fn handle_close_response(header: ObjectHeader) -> Option<FileReadTask> {
+        let obj = match header.details {
+            HeaderDetails::TwoByteFreeFormat(_, FreeFormatVariation::Group70Var4(obj)) => obj,
+            _ => {
+                tracing::warn!(
+                    "File READ response contains unexpected variation: {}",
+                    header.variation
+                );
+                return None;
+            }
+        };
+
+        if obj.status_code != FileStatus::Success {
+            tracing::warn!(
+                "Unable to close file (status code == {:?})",
+                obj.status_code
+            );
+        }
+
+        None
     }
 }
 
@@ -102,14 +292,10 @@ fn write_open(
     writer.write_free_format(&obj)
 }
 
-fn write_read(
-    handle: FileHandle,
-    block: BlockNumber,
-    writer: &mut HeaderWriter,
-) -> Result<(), WriteError> {
+fn write_read(rs: ReadState, writer: &mut HeaderWriter) -> Result<(), WriteError> {
     let obj = Group70Var5 {
-        file_handle: handle.0,
-        block_number: block.0,
+        file_handle: rs.handle.0,
+        block_number: rs.block.0,
         file_data: &[],
     };
     writer.write_free_format(&obj)
