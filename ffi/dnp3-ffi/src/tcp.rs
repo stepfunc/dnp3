c653ea1dc2dd17d2074182b222821a056c15b3cc
jadamcrain <jadamcrain@gmail.com>
2023-04-11 17:55:20 -0700

    respect tls feature

--- ffi/dnp3-ffi/src/tcp.rs
+++ ffi/dnp3-ffi/src/tcp.rs
@@ -6,6 +6,8 @@ pub(crate) fn disable_server_tcp_no_delay() {
     dnp3::tcp::disable_server_tcp_no_delay();
 }
 
+#[cfg(feature = "tls")]
 pub(crate) fn enable_client_name_wildcards() {
+    #[cfg(feature = "tls")]
     dnp3::tcp::tls::dangerous::enable_client_name_wildcards()
 }
