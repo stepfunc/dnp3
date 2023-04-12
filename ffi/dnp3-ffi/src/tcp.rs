3cb1e856efc40803739c8baa02c71edc94059779
jadamcrain <jadamcrain@gmail.com>
2023-04-11 17:44:29 -0700

    Add TlsSettings.EnableClientNameWildcards to FFI

--- ffi/dnp3-ffi/src/tcp.rs
+++ ffi/dnp3-ffi/src/tcp.rs
@@ -5,3 +5,7 @@ pub(crate) fn disable_client_tcp_no_delay() {
 pub(crate) fn disable_server_tcp_no_delay() {
     dnp3::tcp::disable_server_tcp_no_delay();
 }
+
+pub(crate) fn enable_client_name_wildcards() {
+    dnp3::tcp::tls::dangerous::enable_client_name_wildcards()
+}
