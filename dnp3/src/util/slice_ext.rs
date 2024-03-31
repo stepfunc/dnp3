020f14b5766d8750df2bb452a3388a4c46b395aa
jadamcrain <jadamcrain@gmail.com>
2024-03-31 12:40:34 -0700

    remove dead code via beta lints

--- dnp3/src/util/slice_ext.rs
+++ dnp3/src/util/slice_ext.rs
@@ -5,15 +5,10 @@ use crate::link::error::LogicError;
 pub(crate) trait SliceExtNoPanic<T> {
     fn np_split_at(&self, pos: usize) -> Result<(&[T], &[T]), LogicError>;
     fn np_split_at_no_error(&self, pos: usize) -> (&[T], &[T]);
-    fn np_get(&self, range: Range<usize>) -> Result<&[T], LogicError>;
-    fn np_take(&self, count: usize) -> Result<&[T], LogicError> {
-        self.np_get(0..count)
-    }
 }
 
 pub(crate) trait MutSliceExtNoPanic<T> {
     fn np_get_mut(&mut self, range: Range<usize>) -> Result<&mut [T], LogicError>;
-    fn np_skip_mut(&mut self, count: usize) -> Result<&mut [T], LogicError>;
 }
 
 impl<T> SliceExtNoPanic<T> for &[T] {
@@ -30,26 +25,12 @@ impl<T> SliceExtNoPanic<T> for &[T] {
             _ => (self, &[]),
         }
     }
-
-    fn np_get(&self, range: Range<usize>) -> Result<&[T], LogicError> {
-        match self.get(range) {
-            Some(x) => Ok(x),
-            None => Err(LogicError::BadSize),
-        }
-    }
 }
 
 impl<T> MutSliceExtNoPanic<T> for &mut [T] {
     fn np_get_mut(&mut self, range: Range<usize>) -> Result<&mut [T], LogicError> {
         match self.get_mut(range) {
             Some(x) => Ok(x),
-            None => Err(LogicError::BadSize),
-        }
-    }
-
-    fn np_skip_mut(&mut self, count: usize) -> Result<&mut [T], LogicError> {
-        match self.get_mut(count..) {
-            Some(remainder) => Ok(remainder),
             None => Err(LogicError::BadSize),
         }
     }
