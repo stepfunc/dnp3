2d5ad9580f0baf78dd8cff27b33b3eebcb4ed0eb
jadamcrain <jadamcrain@gmail.com>
2025-04-28 10:26:36 -0700

    remove bindgen stuff from Cross, we'll just use ring for platforms that are hard to build w/ Ring

--- install-bindgen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-apt-get update && apt-get -y install clang
-curl -LO https://github.com/stepfunc/bindgen-static/raw/refs/heads/exe/bindgen
-chmod +x bindgen
-mv bindgen /usr/local/bin
