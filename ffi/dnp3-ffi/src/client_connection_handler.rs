9ad9197f4e1e11a3f9f4a66033bbaf87a9bb3d94
jadamcrain <jadamcrain@gmail.com>
2025-08-04 12:04:53 -0700

    initial FFI impl

--- ffi/dnp3-ffi/src/client_connection_handler.rs
+++ ffi/dnp3-ffi/src/client_connection_handler.rs
@@ -1,5 +1,5 @@
 use crate::ffi::ParamError;
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
 use std::net::SocketAddr;
 use std::str::FromStr;
 use std::time::Duration;
@@ -77,4 +77,86 @@ pub(crate) unsafe fn next_endpoint_action_sleep_for(
 
     action.action = Some(NextEndpointActionType::SleepFor(duration));
     ParamError::Ok
+}
+
+pub struct ClientConnectionHandlerAdapter {
+    callback: crate::ffi::ClientConnectionHandler,
+    span_name: String,
+}
+
+impl ClientConnectionHandlerAdapter {
+    pub fn new(callback: crate::ffi::ClientConnectionHandler, span_name: String) -> Self {
+        Self {
+            callback,
+            span_name,
+        }
+    }
+}
+
+impl dnp3::tcp::ClientConnectionHandler for ClientConnectionHandlerAdapter {
+    fn endpoint_span_name(&self) -> String {
+        self.span_name.clone()
+    }
+
+    fn disconnected(&mut self, addr: std::net::SocketAddr, hostname: Option<&str>) -> Duration {
+        use std::ffi::CString;
+
+        let addr_str = CString::new(addr.to_string()).unwrap_or_default();
+        let hostname_str = CString::new(hostname.unwrap_or("")).unwrap_or_default();
+
+        self.callback
+            .disconnected(&addr_str, &hostname_str)
+            .unwrap_or_else(|| Duration::from_secs(5)) // Default to 5 seconds if callback not set
+    }
+
+    fn next(&mut self) -> Result<dnp3::tcp::ConnectionInfo, Duration> {
+        let mut action = NextEndpointAction::new();
+        self.callback.next(&mut action);
+
+        match action.action {
+            Some(NextEndpointActionType::ConnectTo {
+                endpoint,
+                timeout,
+                local_endpoint,
+            }) => {
+                let endpoint = dnp3::tcp::Endpoint::from(endpoint);
+                let mut info = dnp3::tcp::ConnectionInfo::new(endpoint);
+                if let Some(timeout) = timeout {
+                    info.set_timeout(timeout);
+                }
+                if let Some(local) = local_endpoint {
+                    info.set_local_endpoint(local);
+                }
+                Ok(info)
+            }
+            Some(NextEndpointActionType::SleepFor(duration)) => Err(duration),
+            None => Err(Duration::from_secs(5)), // fallback if no action was set
+        }
+    }
+
+    fn connecting(&mut self, addr: SocketAddr, hostname: Option<&str>) {
+        let addr_str = CString::new(addr.to_string()).unwrap_or_default();
+        let hostname_str = CString::new(hostname.unwrap_or("")).unwrap_or_default();
+
+        self.callback.connecting(&addr_str, &hostname_str);
+    }
+
+    fn connect_failed(&mut self, addr: SocketAddr, hostname: Option<&str>) {
+        let addr_str = CString::new(addr.to_string()).unwrap_or_default();
+        let hostname_str = CString::new(hostname.unwrap_or("")).unwrap_or_default();
+
+        self.callback.connect_failed(&addr_str, &hostname_str);
+    }
+
+    fn connected(&mut self, addr: SocketAddr, hostname: Option<&str>) {
+        let addr_str = CString::new(addr.to_string()).unwrap_or_default();
+        let hostname_str = CString::new(hostname.unwrap_or("")).unwrap_or_default();
+
+        self.callback.connected(&addr_str, &hostname_str);
+    }
+
+    fn resolution_failed(&mut self, host_name: &str) {
+        let hostname_str = CString::new(host_name).unwrap_or_default();
+        self.callback.resolution_failed(&hostname_str);
+    }
 }
